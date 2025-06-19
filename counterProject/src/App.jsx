2a6646c97ce1d83262f07d985cf7dba6ad43c5a9
orewagaurav <gauravkumar47967@gmail.com>
2025-06-19 14:57:11 +0530

    project1

--- counterProject/src/App.jsx
+++ counterProject/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(15);
+  // let counter  = 15
 
+  const addValue = () => {
+    counter += 1;
+    console.log("Increased", counter);
+    setCounter(counter);
+  };
+  const decreaseValue = ()=>{
+    counter -= 1
+    console.log("Decreased", counter);
+    setCounter(counter)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>counter project</h1>
+      <h2>Counter Value:{counter}</h2>
+      <button onClick={addValue}>Increment Value</button>
+      &nbsp;&nbsp;
+      <button onClick={decreaseValue}>Decrement Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
