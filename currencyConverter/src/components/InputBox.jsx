d4fb0c62b63f510f19625a5aed338c0ec14d1c0d
orewagaurav <gauravkumar47967@gmail.com>
2025-06-23 17:42:55 +0530

    currency Exchange

--- currencyConverter/src/components/InputBox.jsx
+++ currencyConverter/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+import react,{useId}  from 'react'
 
 
 function InputBox({
@@ -17,7 +17,7 @@ function InputBox({
    
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label  className="text-black/40 mb-2 inline-block">
                     {label}
