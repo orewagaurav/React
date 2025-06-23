c4ef3aa16941a0474fa660f4490362f21a306910
orewagaurav <gauravkumar47967@gmail.com>
2025-06-23 23:47:51 +0530

    React Router

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500   bg-blue-700 flex justify-center items-center w-full min-h-screen' >React Router </h1>
+    </div>
+
+  )
+}
+
+export default App
