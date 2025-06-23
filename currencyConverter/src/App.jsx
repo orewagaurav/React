6c96d03de7df5bd4ff0a663d7b8c3dd1c2c7b2a8
orewagaurav <gauravkumar47967@gmail.com>
2025-06-23 12:09:24 +0530

    currencyCounter

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center text-3xl text-red-500'>
+      Hello world
+    </div>
+  )
+}
+
+export default App
