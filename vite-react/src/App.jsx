aa4a5e8174ba1f86b3698a681bda90f8cc27ec5b
orewagaurav <gauravkumar47967@gmail.com>
2025-06-27 14:30:46 +0530

    useEffect

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,6 +1,12 @@
 import Test from "./test"
+import React, { useState,useEffect } from 'react'
+
 
 function App() {
+  const [count,setCount] = useState(0)
+  const effect  = useEffect(()=>{
+    alert("Welcome to my Page !")
+  },[count])
   return (
     <>
       <Test/>
@@ -8,6 +14,7 @@ function App() {
       <p>orenawa kaizokuni naruhodu</p>
       <h2>Ohayo Gozaimasu</h2>
       <h1>Zoro</h1>
+      <button onClick={()=>setCount(count+1)}>{count}</button>
     </>
   )
 }
