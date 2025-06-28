e97fd2e6b77e088969b542367ff5dd049d5b82d8
orewagaurav <gauravkumar47967@gmail.com>
2025-06-28 12:31:05 +0530

    event handler

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -3,15 +3,25 @@ import React, { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [form,setForm] = useState({email:"",phone:""})
+  const [val,setVal] = useState("Value")
   
+  // const handelChange = (e)=>{
+  //   setForm({...form,[e.target.name]:e.target.value})
+  //   console.log(form);
+    
+  // }
+
   const handelChange = (e)=>{
     setForm({...form,[e.target.name]:e.target.value})
-    console.log(form);
-    
+    console.log(form)
   }
-
   return (
     <>
+    <input type="text" name="email" value = {form.email} onChange={handelChange} />
+    &nbsp;&nbsp;
+    <input type="text" name="phone" value = {form.phone} onChange={handelChange} />
+    &nbsp;&nbsp;
+    <input type="text"  value={val} onChange={(e)=>setVal(e.target.value)}/>
     </>
   )
 }
